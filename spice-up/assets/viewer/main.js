b776981d3881d21485d16b01da3461f991ede0fd
Felipe Escoto <felescoto95@hotmail.com>
2017-10-01 11:36:25 -0500

    Remove extra JS

--- spice-up/assets/viewer/main.js
+++ spice-up/assets/viewer/main.js
@@ -104,23 +104,3 @@ $('#the-file-input').change(function() {
     // grab the first image in the FileList object and pass it to the function
     renderSlides (this.files[0])
 });
-
-
-var inputs = document.querySelectorAll( '.inputfile' );
-Array.prototype.forEach.call( inputs, function( input ) {
-	var label	 = input.nextElementSibling,
-		labelVal = label.innerHTML;
-
-	input.addEventListener( 'change', function( e ) {
-		var fileName = '';
-		if( this.files && this.files.length > 1 )
-			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-		else
-			fileName = e.target.value.split( '\\' ).pop();
-
-		if( fileName )
-			label.querySelector( 'span' ).innerHTML = fileName;
-		else
-			label.innerHTML = labelVal;
-	});
-});
