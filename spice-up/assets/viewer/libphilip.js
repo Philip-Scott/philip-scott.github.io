d6a21d8afc4607ad7850bd09b2480a58fbce893c
Felipe Escoto <felescoto95@hotmail.com>
2017-10-02 09:49:59 -0500

    Fix UTF-8 decoding

--- spice-up/assets/viewer/libphilip.js
+++ spice-up/assets/viewer/libphilip.js
@@ -10,7 +10,9 @@ if (!String.build) {
 }
 
 function base64Decode(str) {
-    return atob(str);
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 }
 
 function makeIdEditable (id) {
